b7acb518fe10f316806dfb36797a7d888b862b73
nashtash <natalie.sablowski@gmail.com>
2019-12-04 05:20:43 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/17GX03WgYuKhWRNkVr-7x4DDhqh9smYEPU_2IfZFkszY/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1d75P92O-odc2fJNM2mkFRoq0aOE4sUs-8VsH89UC09o/edit#gid=603666405';
